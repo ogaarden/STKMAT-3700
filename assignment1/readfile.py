150804964ea01ad6e7758842a7845eedc967645f
Gaarden <115877623+ogaarden@users.noreply.github.com>
2023-08-29 23:51:28 +0200

    Improved the code and added gaussian curve
    Easier to read, added gaussian curve, close prices only.

--- assignment1/readfile.py
+++ assignment1/readfile.py
@@ -1,43 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# R_t = (S_t-S_{t-1}/S_{t-1})
-#Returns R1,R2,R3...R 
-#SD = volatility
+path = "assignment1/meta_data/meta1y_daily.txt"
 
-path = "assignment1\meta_data\meta1y_daily.txt"
-
-# Åpner filen med navnet 'filename.txt' i lesemodus ('r')
 with open(path, 'r') as file:
     file.readline()
-    price_open = []
     price_close = []
     for line in file:
-        price_open.append(float(line.split(",")[1]))
         price_close.append(float(line.split(",")[5]))
 
-daily_profit = []
-trading_days = len(price_open)
+trading_days = len(price_close)
 
+# Calculating daily returns, R = R_t+1 - R_t / R_t
+daily_returns = [(price_close[i + 1] - price_close[i]) / price_close[i] for i in range(trading_days - 1)]
 
-for i in range(trading_days):
-    daily_profit.append((price_close[i]-price_open[i])/price_open[i])
+# Calculating expected return (mean)
+u = sum(daily_returns) / trading_days
 
-def expected_return(trading_days):
-    return sum(daily_profit)/trading_days
+# Calculating volatility (standard deviation)
+k = np.sqrt(sum([(r - u)**2 for r in daily_returns]) / trading_days)
+x = np.linspace(0,trading_days,trading_days)
 
-u = expected_return(trading_days)
+def gaussian_curve(x, k, u):
+    return (1/(np.sqrt(2*np.pi*k*k)))*np.exp(-((x-u)**2)/(2*k*k))
 
-k = 0
-for i in range(trading_days):
-    k+= np.sqrt(((daily_profit[i]-u)**2)/trading_days)
+x = np.linspace(min(daily_returns), max(daily_returns), 100)  # Choose a relevant scale
+y = gaussian_curve(x, k, u)
 
-print(k)
-
-average_price = lambda price, days: sum(price)/days
-
-
-[plt.scatter(i, price_close[i]) for i in range(trading_days)]
+# Plotting
+plt.figure(figsize=(10, 6))
+plt.hist(daily_returns, bins=50, density=True, alpha=0.6, color='g')  # Histogram of daily returns
+plt.plot(x, y, linewidth=2, color='r')  # Gaussian curve
+plt.title('Daily Returns and Fitted Gaussian Curve')
+plt.xlabel('Daily Return')
+plt.ylabel('Density')
 plt.show()
-
-
